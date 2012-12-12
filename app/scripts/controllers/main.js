a532544faa4f5384806f53480c415ec8eef89b6d
mranosa <mark.ranosa@gmail.com>
2012-12-13 01:56:47 +0800

    Igor updates broke the minifier, had to update inject syntax style

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-devApp.controller('MainCtrl', function($scope) {
+devApp.controller('MainCtrl', ['$scope', function($scope) {
   $scope.awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
     'Testacular'
   ];
-});
+}]);
